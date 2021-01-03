a8e6701d1481dc0aec0137ec74880ada9069c041
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-01-03 12:48:35 +0000

    Created new cards

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,31 +3,41 @@ import React from 'react'
 import { CardMedia, CardContent, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 import useStyles from './productStyles';
-// import classes from '*.module.css';
+import './productStyles.css'
 
 const Product = ({product, onAddToCart}) => {
-    const classes = useStyles();
+    // const classes = useStyles();
+    const regex = /(<([^>]+)>)/ig;
+    const description = product.description.replace(regex, ''); 
 
     return (
-        <Card className={classes.root} >
-            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
-            <CardContent>
-                <div className={classes.cardContent}>
-                    <Typography variant="h5" gutterBottom>
-                        {product.name}
-                    </Typography>
-                    <Typography variant="h5">
-                        {product.price.formatted_with_symbol}
-                    </Typography>
-                </div>
-                <Typography dangerouslySetInnerHTML={{ __html: product.description} } variant="body2" color="textSecondary" />
-            </CardContent>
-            <CardActions disableSpacing className={classes.CardActions}>
-                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
-                    <AddShoppingCart />
-                </IconButton>
-            </CardActions>
-        </Card>
+        // <Card className={`${classes.root} shan`} >
+        //     <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+        //     <CardContent>
+        //         <div className={classes.cardContent}>
+        //             <Typography variant="h5" gutterBottom>
+        //                 {product.name}
+        //             </Typography>
+        //             <Typography variant="h5">
+        //                 {product.price.formatted_with_symbol}
+        //             </Typography>
+        //         </div>
+        //         <Typography dangerouslySetInnerHTML={{ __html: product.description} } variant="body2" color="textSecondary" />
+        //     </CardContent>
+        //     <CardActions disableSpacing className={classes.CardActions}>
+        //         <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
+        //             <AddShoppingCart />
+        //         </IconButton>
+        //     </CardActions>
+        // </Card>
+
+        <div className="card">
+            <img src={product.media.source} alt={product.name} style={{width: '100%'}} className='image' />
+            <h1>{product.name}</h1>
+            <p className="price">{product.price.formatted_with_symbol}</p>
+            <p>{description} </p>
+            <p><button onClick={() => onAddToCart(product.id, 1)}>Add to Cart</button></p>
+        </div>
 
 
     )
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,23 +2,36 @@ import React from 'react'
 import Product from './Product/Product'
 import { Grid } from '@material-ui/core'
 import useStyles from './productsStyles'
+import './productsStyles.css'
 
 
 const Products = ({products, onAddToCart}) => {;
-    const classes = useStyles()
+    // const classes = useStyles()
     return (
-        <main className={classes.content}>
-            <div className={classes.toolbar}/>
-            <Grid container justify="center" spacing={4}>
-                {products.map((product) => {
+        // <main className={classes.content}>
+        //     <div className={classes.toolbar}/>
+        //     <Grid container justify="center" spacing={4}>
+        //         {products.map((product) => {
+        //             return (
+        //             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+        //                 <Product product={product} onAddToCart={onAddToCart}/>
+        //             </Grid>
+        //             )
+        //         })}
+        //     </Grid>
+        // </main>
+        <div className='container'>
+            <div className='top-spacing' />
+            <div className='sub-container'>
+            {products.map((product) => {
                     return (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <div key={product.id} className='product'>
                         <Product product={product} onAddToCart={onAddToCart}/>
-                    </Grid>
+                    </div>
                     )
                 })}
-            </Grid>
-        </main>
+            </div>
+        </div>
     )
 }
 
